fa1774d7144bb83d14e2640606bd8ced098bdd7c
AyubuAyubu <ayubu6143@gmail.com>
2022-08-25 16:45:58 +0300

    Complete

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let re = /[^A-Za-z0-9]/g
+  str = str.toLowerCase().replace(re, '')
+  var len = str.length
+  for (var i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false
+    }
+  }
+  return true
 }
 
 /* 
